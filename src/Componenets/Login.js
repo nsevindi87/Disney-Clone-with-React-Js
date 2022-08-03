9dfc61f67b99e43418b4689e95a3a7d21f7d5056
Nizami Sevindi <nsevindi87@gmail.com>
2022-08-03 18:55:45 +0200

    yarim kaldi - styled.components yuklenemedi

--- src/Componenets/Login.js
+++ src/Componenets/Login.js
@@ -1,7 +1,22 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
 const Login =(props)=>{
-    return <div>Login</div>
+    return (
+        <Container>
+            <Content>Content</Content>
+        </Container>
+    )
 }
+
+const Container = styled.section`
+  color: '#fff';
+  background-color: purple;
+  padding: 8px 16px;
+  border-radius: 4px
+`
+const Content = styled.section`
+color: white;
+`
+;
 
 export default Login;
